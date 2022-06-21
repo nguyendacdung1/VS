196838a0ce4449cb77917e6c60399c9d37bacefc
nguyendacdung1 <nguyendungmk9@gmail.com>
2022-06-21 09:22:28 +0700

    vs

---
+++ First_prg/First_prg/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Example
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("This is the my first program using C#");
+        Console.ReadLine();
+    }
+}
